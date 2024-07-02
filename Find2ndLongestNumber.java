489e6b7c728a2e6fb1d3c0c1580d44cf73b840ec
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-02 23:40:17 +0530

    Create Find2ndLongestNumber.java

---
+++ Find2ndLongestNumber.java
@@ -0,0 +1,10 @@
+public class Find2ndLongestNumber {
+    public static void main(String[] args) {
+    System.out.println("hello");
+    int a[] = {1,2,3,6,75,9,81,23,45,22};
+
+    // Using Inbuild function
+    // Arrays.sort(a);
+    // System.out.println(a[a.length-2]);
+    }
+}
