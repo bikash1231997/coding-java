bc372a02d73b56bd030c9224668c6d85bd576ad3
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-03 00:32:48 +0530

    Update Find2ndLongestNumber.java

--- Find2ndLongestNumber.java
+++ Find2ndLongestNumber.java
@@ -1,10 +1,19 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Find2ndLongestNumber {
     public static void main(String[] args) {
     System.out.println("hello");
     int a[] = {1,2,3,6,75,9,81,23,45,22};
 
-    // Using Inbuild function
+    // // Using Inbuild function
     // Arrays.sort(a);
     // System.out.println(a[a.length-2]);
-    }
+
+    // using Collection
+    List<Integer> b = Arrays.stream(a).boxed().toList();
+    Collections.sort(b);
+    int c = b.get(b.size() -2);
+    System.out.println(c);
 }
