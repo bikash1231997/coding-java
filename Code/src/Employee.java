b1364278789498008a922c0ec0ed1c864d85d100
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-04 23:49:44 +0530

    Code UsingIDE

---
+++ Code/src/Employee.java
@@ -0,0 +1,30 @@
+public class Employee {
+
+    private Long id;
+    private String name;
+    private Long salary;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getSalary() {
+        return salary;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(Long salary) {
+        this.salary = salary;
+    }
+}
---
+++ Code/src/FindAvgSalryEmployee.java
@@ -0,0 +1,10 @@
+public class FindAvgSalryEmployee {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        Employee e = new Employee();
+        e.setId(1L);
+        e.setName("Hello");
+        e.setSalary(1000L);
+    }
+}
