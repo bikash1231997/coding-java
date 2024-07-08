984f4d649c6c117b1bcd529c87f67fa603e530a4
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-08 12:50:23 +0530

    Update FindSqureNunber.java

--- Code/src/FindSqureNunber.java
+++ Code/src/FindSqureNunber.java
@@ -1,10 +1,15 @@
 //From a list of numbers, take square of those numbers and collect them in a new List using Streams collect method
-public class FindLongestNumber {
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindSqureNunber {
 
    public static void main(String[] args) {
       System.out.println("hello");
      List<Integer> numbers = Arrays.asList(1, 4, 9, 16, 25, 36, 49, 64, 81, 100);
-     List<Integer> squaredNum = numbers.stream().map(n -> n *n).collect(Collector.toList());
+     List<Integer> squaredNum = numbers.stream().map(n -> n *n).collect(Collectors.toList());
      System.out.println(numbers);
      System.out.println(squaredNum);
    }
