2715c7b3458fd7189cd80d58a12170c3bea033f3
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-09 17:29:06 +0530

    Update FindSqureNunber.java

--- Code/src/FindSqureNunber.java
+++ Code/src/FindSqureNunber.java
@@ -8,9 +8,9 @@ public class FindSqureNunber {
 
    public static void main(String[] args) {
       System.out.println("hello");
-     List<Integer> numbers = Arrays.asList(1, 4, 9, 16, 25, 36, 49, 64, 81, 100);
-     List<Integer> squaredNum = numbers.stream().map(n -> n *n).collect(Collectors.toList());
-     System.out.println(numbers);
-     System.out.println(squaredNum);
+      List<Integer> numbers = Arrays.asList(1, 4, 9, 16, 25, 36, 49, 64, 81, 100);
+      List<Integer> squaredNum = numbers.stream().map(n -> n * n).collect(Collectors.toList());
+      System.out.println(numbers);
+      System.out.println(squaredNum);
    }
 }
