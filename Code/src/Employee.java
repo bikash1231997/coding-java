c3be564bad9503231899c7d42b415cbd179a4dc4
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-10 00:24:46 +0530

    Update Employee.java

--- Code/src/Employee.java
+++ Code/src/Employee.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,6 +8,7 @@ public class Employee {
     private String name;
     private int salary;
     private String gender;
+    private LocalDate joinDate;
 
     public Long getId() {
         return id;
@@ -22,6 +24,10 @@ public class Employee {
 
     public String getGender() {
         return gender;
+    }
+
+    public LocalDate getJoinDate() {
+        return joinDate;
     }
 
     public void setId(Long id) {
@@ -40,6 +46,10 @@ public class Employee {
         this.gender = gender;
     }
 
+    public void setJoinDate(LocalDate joinDate) {
+        this.joinDate = joinDate;
+    }
+
     public List<Employee> getEmployeeList() {
 
         Employee e1 = new Employee();
@@ -47,31 +57,36 @@ public class Employee {
         e1.setName("Hello");
         e1.setSalary(1000);
         e1.setGender("Female");
+        e1.setJoinDate(LocalDate.of(2015, 8, 2));
 
         Employee e2 = new Employee();
         e2.setId(2L);
         e2.setName("Hello2");
         e2.setSalary(2000);
         e2.setGender("Male");
+        e2.setJoinDate(LocalDate.of(2016, 8, 2));
 
         Employee e3 = new Employee();
         e3.setId(3L);
         e3.setName("Hello3");
         e3.setSalary(3000);
         e3.setGender("Female");
+        e1.setJoinDate(LocalDate.of(2017, 8, 2));
 
         Employee e4 = new Employee();
         e4.setId(4L);
         e4.setName("Hello4");
         e4.setSalary(4000);
         e4.setGender("Female");
+        e1.setJoinDate(LocalDate.of(2018, 8, 2));
 
         Employee e5 = new Employee();
         e5.setId(5L);
         e5.setName("Hello5");
         e5.setSalary(5000);
         e5.setGender("Male");
-        
+        e1.setJoinDate(LocalDate.of(2019, 8, 2));
+
         return Arrays.asList(e1, e2, e3, e4, e5);
     }
 
