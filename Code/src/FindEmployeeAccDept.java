dbed782b960071431edfe815acb553aef8fbe511
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-11 23:27:54 +0530

    Create FindEmployeeAccDept.java

---
+++ Code/src/FindEmployeeAccDept.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * FindEmployeeAccodingToTheDept
+ */
+public class FindEmployeeAccDept {
+
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        List<Employee> employeeList = e.getEmployeeList();
+        Map<String, Long> collect = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+        collect.entrySet().forEach(t -> System.out.println(t.getKey() + " " + t.getValue()));
+    }
+
+}
