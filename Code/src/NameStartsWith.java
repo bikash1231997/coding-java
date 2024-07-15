ce3b6c2472f5a8a1c5014435b495372faf812b30
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-15 23:29:38 +0530

    Update NameStartsWith.java

--- Code/src/NameStartsWith.java
+++ Code/src/NameStartsWith.java
@@ -1,1 +1,20 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Find Employee whose Name Starts With
+ */
+public class NameStartsWith {
+
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        List<Employee> employeeList = e.getEmployeeList();
+        List<String> nameStarsWith = employeeList.stream().filter(t -> t.getName().startsWith("A"))
+                .map(Employee::getName)
+                .collect(Collectors.toList());
+        for (String name : nameStarsWith) {
+            System.out.println(name);
+        }
+    }
+}
 
