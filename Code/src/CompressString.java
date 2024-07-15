6baa40f8c7ffb645b343b55a74ad1f5da700f175
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-15 23:24:00 +0530

    Create CompressString.java

---
+++ Code/src/CompressString.java
@@ -0,0 +1,23 @@
+/**
+ * Compress a String
+ */
+public class CompressString {
+    public static void main(String[] args) {
+        String s = "aaabbbbbbbbceeeeisssllaaaaabbbbrr"; 
+        String s2 = "";
+        char[] ch = s.toCharArray();
+        int counter = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if( i+1 < s.length() && ch[i]==ch[i+1]){
+                counter += 1;
+            } else {
+                s2 += ch[i];
+                if(counter >1){
+                    s2 += counter;
+                }
+                counter = 1;
+            }
+        }
+        System.out.println(s2);
+    }
+}
