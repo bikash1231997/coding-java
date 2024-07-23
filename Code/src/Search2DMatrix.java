c87e952395550ae53abbd6247ef527abbe292d03
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-23 13:25:20 +0530

    Create Search2DMatrix.java

---
+++ Code/src/Search2DMatrix.java
@@ -0,0 +1,8 @@
+ // Search a 2D Matrix
+// https://leetcode.com/problems/search-a-2d-matrix/description/
+// Matrix [[1,3,5,7],[10,11,16,20],[23,30,34,60]] target 2
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+    }
+}
