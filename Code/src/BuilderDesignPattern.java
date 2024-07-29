37d743f1469c7c744af995708c9367eaabecaeb2
Bikash Mahapatra <bikashmohapatra1997@gmail.com>
2024-07-29 16:29:46 +0530

    Create BuilderDesignPattern.java

---
+++ Code/src/BuilderDesignPattern.java
@@ -0,0 +1,41 @@
+public class Employee {
+
+    private String name;
+    private String company;
+
+    private Employee(EmployeeBuilder employeeBuilder) {
+        name = employeeBuilder.name;
+        company = employeeBuilder.company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public static class EmployeeBuilder {
+        private String name;
+        private String company;
+       
+        //constructor for required fields
+        public EmployeeBuilder(String name, String company) {
+            this.name = name;
+            this.company = company;
+        }
+
+        //Build the Employee object
+        public Employee build() {
+            return new Employee(this);
+        }
+    }
+}
+
+class TestBuilder {
+    public static void main(String[] args) {
+        //Building the object of Employee thru the build() method provided in EmployeeBuilder class.
+        Employee employee = new Employee.EmployeeBuilder("Vikram", "ABC").setHasBike(false).setHasBike(true).build();
+    }
+}
